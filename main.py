5377a879087832dc5aa32a97fe8a0720706e9495
Ritchie5 <80743835+Ritchie5@users.noreply.github.com>
2021-03-31 12:52:02 +0800

    Added Dijksta Algorithm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 import time
-import timeit
+from IndexMinPQ import IndexMinPQ
 
 
 class Edge:
@@ -139,15 +139,93 @@ def find_shortest_path(graph1, start, end, path=[], time=0):
     return paths
 
 
-def main():
+class Dijkstra:
+    source = None
+    list = {}
+    pq = None
 
+    def __init__(self, G, source):
+        self.source = source
+
+        for v in G.adjList:
+            self.list[v] = []
+            self.list[v].append(Dijkstra_node(None, False, 9999))
+
+        self.pq = IndexMinPQ()
+        self.pq.insert(source, 0.0)
+
+        for node in self.list[source]:
+            node.dist = 0.0
+
+        while not self.pq.isEmpty():
+            v = self.pq.del_min()
+            self.pq.print_pq()
+            for node in self.list[v]:
+                node.marked = True
+            print("Relaxing Vertex " + str(v))
+            for e in G.adjList[v]:
+                self.relax(e)
+
+    def relax(self, e):
+        v = e.src()
+        w = e.dest()
+
+        for node in self.list[v]:
+            source_node = node
+
+        for node in self.list[w]:
+            dest_node = node
+
+        if dest_node.dist > source_node.dist + int(e.weight):
+            dest_node.dist = source_node.dist + int(e.weight)
+            dest_node.edge = e
+
+            if self.pq.contains(w):
+                self.pq.change(w, dest_node.dist)
+            elif dest_node.marked is False:
+                self.pq.insert(w, dest_node.dist)
+
+    def print_path(self, dest):
+        path = [dest]
+        for node in self.list[dest]:
+            time = node.dist
+
+        while dest != self.source:
+            for node in self.list[dest]:
+                temp = node.edge
+                temp = temp.src()
+                path.append(temp)
+                dest = temp
+
+        path.reverse()
+        print(path)
+        print("Time take:",time)
+
+
+class Dijkstra_node:
+    edge = None
+    marked = False
+    dist = 0
+
+    def __init__(self, edge, marked, dist):
+        self.edge = edge
+        self.marked = marked
+        self.dist = dist
+
+    def edge(self):
+        return self.edge.src()
+
+
+def main():
     graph = Graph("C:/1008/graph.csv")
     # temp1 = find_path(graph.adjList, "Pasir ris", "Woodlands")
     # print(temp1)
-    temp2 = compare_shortest_path(graph.adjList, "Pasir ris", "Woodlands")
-    print(temp2)
+    #temp2 = compare_shortest_path(graph.adjList, "Pasir ris", "Woodlands")
+    #print(temp2)
     # temp3 = find_all_path(graph.adjList, "Pasir ris", "Woodlands")
     # print(temp3)
+    temp4 = Dijkstra(graph, "Simei")
+    temp4.print_path("64229:72")
 
 
 if __name__ == '__main__':
@@ -155,4 +233,3 @@ if __name__ == '__main__':
     main()
     end = time.time()
     print(end - start)
-
