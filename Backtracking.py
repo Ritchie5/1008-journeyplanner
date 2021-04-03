8223f50446594cfd1d4a78b5bb450a50c6959e53
Ritchie5 <80743835+Ritchie5@users.noreply.github.com>
2021-04-04 00:15:26 +0800

    Update Backtracking.py
    Find the shortest path. Elegant.

--- Backtracking.py
+++ Backtracking.py
@@ -31,42 +31,21 @@ class Backtracking:
                 pass
         return paths
 
-    def get_shortest_path(self, graph1, start, end):
-        paths = self.find_shortest_path(graph1, start, end)
-
-        number_of_path = len(paths)
-        time = [0 for i in range(number_of_path)]
-        for x in range(0, number_of_path):
-            for y in range(1, len(paths[x]), 2):
-                if x == 1 and time[x] < time[x-1]:
-                    time[x] += int(paths[x][y])
-
-        time_taken = time[0]
-        fastest_path = 0
-        for x in range(1, number_of_path):
-            if time[x] < time_taken:
-                time_taken = time[x]
-                fastest_path = x
-
-        fastest = []
-        for y in range(0, len(paths[fastest_path]), 2):
-            fastest.append(paths[fastest_path][y])
-        return fastest
-
-    def find_shortest_path(self, graph1, start, end, path=[], time=0):
-        path = path + [start] + [time]
+    def find_shortest_path(self, graph1, start, end, time, path=[0]):
+        path[0] += int(time)
+        path = path + [start]
         if start == end:
             return [path]
         if start not in graph1:
             pass
-        paths = []
+        shortest_path = None
         for node in graph1[start]:
             if node.destination not in path:
                 time = node.weight
-                new_path = self.find_shortest_path(graph1, node.destination, end, path, time)
+                new_path = self.find_shortest_path(graph1, node.destination, end, time, path)
                 if new_path is not None:
-                    for new_path in new_path:
-                        paths.append(new_path)
+                    if shortest_path is None or new_path[0] < shortest_path[0]:
+                        shortest_path = new_path
             if node.destination in path:
                 pass
-        return paths
+        return shortest_path
