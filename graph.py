6ec9d828a3923a22ff4552df901e5e96118142fa
Ritchie5 <80743835+Ritchie5@users.noreply.github.com>
2021-04-07 00:52:06 +0800

    Debugged graph - some edges were added in more than once for some mrt and buses. Fixed the issue.

--- graph.py
+++ graph.py
@@ -16,21 +16,46 @@ class Graph:
                 e = Edge(v1, v2, v3)
                 e1 = Edge(v2, v1, v3)
 
+                # Uni direction implementation for buses
+                # Check if mrt/bus is already in graph
+                # If in graph add edge/path to mrt/bus key
                 if v1 in self.adjList:
-                    if v2 in self.adjList[v1]:
-                        pass
-                    else:
+                    # Check if edge has already been added to mrt/bus key
+                    edge_in_graph = False
+                    for edge in self.adjList[v1]:
+                        if v2 == edge.dest():
+                            edge_in_graph = True
+
+                    # If edge is not in graph add edge
+                    if not edge_in_graph:
                         self.adjList[v1].append(e)
+
+                    # If edge in graph do not add edge
+                    elif edge_in_graph:
+                        pass
+
+                # Mrt/Bus not found in the graph, add key and edge into graph
                 else:
                     self.adjList[v1] = []
                     self.adjList[v1].append(e)
 
+                # Dual direction
+                # MRT are dual direction
                 if type == "MRT":
                     if v2 in self.adjList:
-                        if v1 in self.adjList[v2]:
-                            pass
-                        else:
+                        # Check if edge has already been added to mrt/bus key
+                        edge_in_graph = False
+                        for edge in self.adjList[v2]:
+                            if v1 == edge.dest():
+                                edge_in_graph = True
+
+                        # If edge is not in graph add edge
+                        if not edge_in_graph:
                             self.adjList[v2].append(e1)
+
+                        # If edge in graph do not add edge
+                        elif edge_in_graph:
+                            pass
                     else:
                         self.adjList[v2] = []
                         self.adjList[v2].append(e1)
@@ -39,6 +64,10 @@ class Graph:
         for line in self.adjList:
             for edge in self.adjList[line]:
                 print(line + " " + edge.to_string())
+
+    def print_dest(self):
+        for edge in self.adjList["Simei"]:
+            print(edge.dest())
 
 
 class Edge:
@@ -61,3 +90,6 @@ class Edge:
         return "[" + str(self.source) + "-" + str(self.destination) + "," + str(self.weight) + "] "
 
 
+if __name__ == '__main__':
+    graph = Graph("graph.csv")
+    graph.print_list()
