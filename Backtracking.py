5e8dbea8c12f5ebe27ef2aab42cd01ea119e82bc
Ritchie5 <80743835+Ritchie5@users.noreply.github.com>
2021-04-07 13:48:44 +0800

    Kenzo, Fixed the positional issue.

--- Backtracking.py
+++ Backtracking.py
@@ -1,6 +1,11 @@
+from graph import Graph
+
+
 class Backtracking:
 
     def find_path(self, graph1, start, end, path=[]):
+        print(start)
+        print(end)
         path = path + [start]
         if start == end:
             return path
@@ -8,7 +13,7 @@ class Backtracking:
             pass
         for node in graph1[start]:
             if node.destination not in path:
-                new_path = self.find_path(graph1, node.destination, end, path)
+                new_path = self.find_path(Backtracking, graph1, node.destination, end, path)
                 if new_path is not None:
                     return new_path
             if node.destination in path:
@@ -23,7 +28,7 @@ class Backtracking:
         paths = []
         for node in graph1[start]:
             if node.destination not in path:
-                new_path = self.find_all_path(graph1, node.destination, end, path)
+                new_path = self.find_all_path(Backtracking, graph1, node.destination, end, path)
                 if new_path is not None:
                     for new_path in new_path:
                         paths.append(new_path)
@@ -41,11 +46,39 @@ class Backtracking:
         shortest_path = None
         for node in graph1[start]:
             if node.destination not in path:
-                time = node.weight
-                new_path = self.find_shortest_path(graph1, node.destination, end, time, path)
+                time = node.time
+                new_path = self.find_shortest_path(Backtracking, graph1, node.destination, end, time, path)
                 if new_path is not None:
                     if shortest_path is None or new_path[0] < shortest_path[0]:
                         shortest_path = new_path
             if node.destination in path:
                 pass
         return shortest_path
+
+    def find_cost_path(self, graph1, start, end, time, cost, path=[0, 0]):
+        path[0] += int(time)
+        path[1] += float(cost)
+        path = path + [start]
+        if start == end:
+            return [path]
+        if start not in graph1:
+            pass
+        shortest_path = None
+        for node in graph1[start]:
+            if node.destination not in path:
+                time = node.time
+                cost = node.cost
+                new_path = self.find_cost_path(Backtracking, graph1, node.destination, end, time, cost, path)
+                if new_path is not None:
+                    if shortest_path is None or new_path[0] < shortest_path[0]:
+                        shortest_path = new_path
+            if node.destination in path:
+                pass
+        return shortest_path
+
+
+if __name__ == '__main__':
+    graph = Graph("C:/1008/graph.csv")
+    back = Backtracking
+    temp = back.find_cost_path(Backtracking, graph.adjList, "Simei", "Orchard", 0, 0)
+    print(temp)
