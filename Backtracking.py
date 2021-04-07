0ffabee39de74a6c6532d4e69b5a904257a10f77
wjayjay123 <77979635+wjayjay123@users.noreply.github.com>
2021-04-08 00:55:47 +0800

    Revert "Merge branch 'main' into GUI-Testing"
    This reverts commit bf494ae991f8c9c61a8d3008c435037361298349, reversing
    changes made to cae1ec6cc6803dc01e76e1e31b324bcedc7beab1.

--- Backtracking.py
+++ Backtracking.py
@@ -3,37 +3,16 @@ from graph import Graph
 
 class Backtracking:
     # Find shortest path based on time
-    def find_shortest_path(self, graph1, start, end, time, cost):
-        paths = self.find_all_path(Backtracking, graph1, start, end, time, cost)
-
-        # Format array
-        arr = [[0, 0] for j in range(len(paths))]
-        for x in range(0, len(paths)):
-            for y in range(0, len(paths[x]), 3):
-                arr[x].append(paths[x][y])
-                arr[x][0] += float(paths[x][y + 1])
-                arr[x][1] += float(paths[x][y + 2])
-
-        shortest_time = arr[0][0]
-        shortest_path = 0
-        for x in range(1, len(arr)):
-            if shortest_time > arr[x][0]:
-                shortest_time = arr[x][0]
-                shortest_path = x
-
-        return arr[shortest_path]
-
-    def find_shortest_path1(self, graph1, start, end, time, cost, path=[0, 0]):
-        # Add Mrt/Bus, time, cost to path array
-        path = path + [start, time, cost]
-
+    def find_shortest_path(self, graph1, start, end, time, cost, path=[0, 0]):
+        path[0] += int(time)        # First element of array will contain time
+        path[1] += float(cost)      # Second element of array will contain cost
+        path = path + [start]       # Third element onwards will contain path
         # Reached destination
         if start == end:
             return [path]
         # Went out of Graph
         if start not in graph1:
             return None
-
         shortest_path = None
 
         # Check all edges of graph
@@ -50,20 +29,19 @@ class Backtracking:
             # If destination already in path do nothing
             elif node.destination in path:
                 pass
-
         # Return the shortest path
         return shortest_path
 
     # Find all possible paths
-    def find_all_path(self, graph1, start, end, time, cost, path=[]):
-        path = path + [start, time, cost]
+    def find_all_path(self, graph1, start, end, time, cost, path=[0, 0]):
+        path[0] += int(time)        # First element of array will contain time
+        path[1] += float(cost)      # Second element of array will contain cost
+        path = path + [start]       # Third element onwards will contain path
         # Reached destination
         if start == end:
             return [path]
         # Went out of Graph
         if start not in graph1:
-            path[0] = 0
-            path[1] = 0
             pass
         all_paths = []              # Array to contain all paths
 
