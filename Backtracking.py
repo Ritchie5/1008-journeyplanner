5e2132a62116a67a90f53292e4fffbbc0d1af78a
wjayjay123 <77979635+wjayjay123@users.noreply.github.com>
2021-04-08 12:20:32 +0800

    test

--- Backtracking.py
+++ Backtracking.py
@@ -42,7 +42,11 @@ class Backtracking:
             if node.destination not in path:
                 time = node.time
                 cost = node.cost
+<<<<<<< HEAD
                 new_path = self.find_shortest_path(graph1, node.destination, end, time, cost, path)
+=======
+                new_path = self.find_shortest_path1(Backtracking, graph1, node.destination, end, time, cost, path)
+>>>>>>> parent of e6e0ac1 (Fixed Backtracking)
                 if new_path is not None:
                     # Check if this is the shortest path
                     if shortest_path is None or new_path[0] < shortest_path[0]:
@@ -73,7 +77,11 @@ class Backtracking:
             if node.destination not in path:
                 time = node.time
                 cost = node.cost
+<<<<<<< HEAD
                 new_path = self.find_all_path(graph1, node.destination, end, time, cost, path)
+=======
+                new_path = self.find_all_path(Backtracking, graph1, node.destination, end, time, cost, path)
+>>>>>>> parent of e6e0ac1 (Fixed Backtracking)
                 if new_path is not None:
                     # append all new paths to all_path array
                     for new_path in new_path:
@@ -89,10 +97,14 @@ if __name__ == '__main__':
     tag = "MRTBUS"
     graph = Graph("graph.csv",tag)
     back_tracker = Backtracking
+<<<<<<< HEAD
     paths = back_tracker.find_shortest_path(Backtracking, graph.adjList, "Simei", "Bedok", 0, 0.0)
 =======
     graph = Graph("graph.csv")
     back_tracker = Backtracking()
     paths = back_tracker.find_all_path(graph.adjList, "Simei", "Orchard", 0, 0)
 >>>>>>> parent of c1f458e (Update Backtracking.py)
+=======
+    paths = back_tracker.find_shortest_path(Backtracking, graph.adjList, "Simei", "Orchard", 0, 0.0)
+>>>>>>> parent of e6e0ac1 (Fixed Backtracking)
     print(paths)
