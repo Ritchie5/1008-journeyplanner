3247112a1fbffd91a3896187831bc27c12c13576
Ritchie5 <80743835+Ritchie5@users.noreply.github.com>
2021-04-08 17:48:50 +0800

    Fixed backtracking

--- Backtracking.py
+++ Backtracking.py
@@ -5,48 +5,40 @@ class Backtracking:
     # Find shortest path based on time
     def find_shortest_path(self, graph1, start, end, time, cost):
         paths = self.find_shortest_path1(graph1, start, end, time, cost)
-
+        print(paths)
         # Format array
         arr = [0, 0]
-        arr[0] = paths[0][0]
-        arr[1] = paths[0][1]
-        for x in range(2, len(paths[0]), 3):
-            arr.append(paths[0][x])
+        arr[0] = paths[0]
+        arr[1] = paths[1]
+        for x in range(2, len(paths), 3):
+            arr.append(paths[x])
 
         return arr
 
     # Find Shortest possible path recursively
-    def find_shortest_path1(self, graph1, start, end, time, cost, path=[0.0, 0.0]):
+    def find_shortest_path1(self, graph, start, end, time, cost, shortestLength = 0, path=[0, 0]):
+        print("called")
         path = path + [start, time, cost]
         path[0] = 0
         path[1] = 0
         for x in range(2, len(path), 3):
             path[0] += float(path[x + 1])
             path[1] += float(path[x + 2])
-        # Reached destination
         if start == end:
-            return [path]
-        # Went out of Graph
-        if start not in graph1:
-            pass
-        shortest = None  # Array to contain all paths
-
-        # Check all edges of graph
-        for node in graph1[start]:
-            # If the destination not already in path, find path
+            return path
+        if start not in graph:
+            return None
+        shortest = None
+        for node in graph[start]:
             if node.destination not in path:
                 time = node.time
                 cost = node.cost
-                if shortest is None or path[0] < shortest[0][0]:
-                    new_path = self.find_shortest_path1(graph1, node.destination, end, time, cost, path)
-                    if new_path is not None:
-                        # append all new paths to all_path array
-                        shortest = new_path
-                        return shortest
-            elif node.destination in path:
-                pass
-
-        # Return all paths
+                if shortestLength == 0 or path[0] < shortestLength:
+                    newpath = self.find_shortest_path1(graph, node.destination, end, time, cost, shortestLength, path)
+                    if newpath:
+                        if not shortest or path[0] < shortestLength:
+                            shortest = newpath
+                            shortestLength = newpath[0]
         return shortest
 
 
